0fa6427847d92fee2f09e05cca8e798517d1bd16
grantwbrooks <grantwbrooks@gmail.com>
2017-08-13 17:20:51 -0400

    finished survey form

---
+++ apps/mysurvey/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+#if using messages be sure to import and possibly terminal stuff for sessions
+from django.contrib import messages
+
+
+def index(request):
+    return render(request, 'mysurvey/index.html')
+
+
+def process(request):
+    if request.method == "POST":
+        print "post it is"
+        request.session['fullform'] = request.POST
+        try:
+            request.session['count'] += 1
+        except:
+            request.session['count'] = 1
+        print request.session['fullform']
+        print request.session['fullform']['yourname']
+        print request.POST
+        
+        messages.success(request, 'Thanks for submitting this form! You have subitted this form ' + str(request.session['count']) + ' times now.')
+        return redirect('/result/')
+
+
+def result(request):
+    return render(request, 'mysurvey/result.html')
+
+
+def goback(request):
+
+    return redirect('/')
+
+def reset(request):
+    try:
+        del request.session['count']
+        # request.session['count'] = 0
+    # this try except is needed just in case the key doesn't exist! Also don't need both del count and count = 0 choose one.
+        return redirect('/')
+    except:
+        return redirect('/')
